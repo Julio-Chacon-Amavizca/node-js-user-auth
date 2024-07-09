30d5585d778261b3fa825e203b7a1b7b74240774
JulioChacon <jcchacon3000@gmail.com>
2024-07-09 16:38:14 -0700

    Create index endponts

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+
+import { PORT, SECRET_JWT } from './config.js'
 import UserRepository from './user-repository.js'
 
 const app = express()
-app.use(express.json())
+
 app.set('view engine', 'ejs')
 
+app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  let data = null
+  req.session = { user: null }
+  try {
+    data = jwt.verify(token, SECRET_JWT)
+    req.session.user = data
+  } catch (error) { }
+
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body
   try {
     const user = await UserRepository.login({ username, password })
-    res.send(user)
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT, {
+      expiresIn: '1h'
+    })
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // no se puede acceder desde el cliente
+        secure: process.env.NODE_ENV === 'production', // solo se envía en HTTPS
+        sameSite: 'strict', // solo se envía si la solicitud es en el mismo sitio
+        maxAge: 1000 * 60 * 60 // 1 hora
+      })
+      .send({ user, token })
   } catch (error) {
     res.status(401).send(error.message)
   }
@@ -33,7 +63,17 @@ app.post('/register', async (req, res) => {
 })
 
 app.post('/logout', (req, res) => {
-  res.send('<h1>Logout</h1>')
+  res
+    .clearCookie('access_token')
+    .json({ message: 'Logged out' })
+})
+
+app.get('/protected', async (req, res) => {
+  const { user } = req.session
+  if (!user) {
+    return res.status(401).send('Unauthorized')
+  }
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
